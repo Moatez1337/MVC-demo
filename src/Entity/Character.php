87a46551190f5fb9607b05201fe347cd86f95dac
moatez1337 <mo3tazz99@gmail.com>
2024-10-08 01:56:40 +0200

    added player entity

---
+++ src/Entity/Character.php
@@ -0,0 +1,156 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+class Character
+{
+    private ?int $id = null;
+
+    private ?string $name = null;
+
+    private ?string $class = null;
+
+    private ?int $level = null;
+
+    private ?int $health = null;
+
+    private ?int $mana = null;
+
+    private ?int $strength = null;
+
+    private ?int $agility = null;
+
+    private ?int $intelligence = null;
+
+    private ?string $gender = null;
+
+    private ?int $experience = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getClass(): ?string
+    {
+        return $this->class;
+    }
+
+    public function setClass(string $class): static
+    {
+        $this->class = $class;
+
+        return $this;
+    }
+
+    public function getLevel(): ?int
+    {
+        return $this->level;
+    }
+
+    public function setLevel(int $level): static
+    {
+        $this->level = $level;
+
+        return $this;
+    }
+
+    public function getHealth(): ?int
+    {
+        return $this->health;
+    }
+
+    public function setHealth(int $health): static
+    {
+        $this->health = $health;
+
+        return $this;
+    }
+
+    public function getMana(): ?int
+    {
+        return $this->mana;
+    }
+
+    public function setMana(int $mana): static
+    {
+        $this->mana = $mana;
+
+        return $this;
+    }
+
+    public function getStrength(): ?int
+    {
+        return $this->strength;
+    }
+
+    public function setStrength(int $strength): static
+    {
+        $this->strength = $strength;
+
+        return $this;
+    }
+
+    public function getAgility(): ?int
+    {
+        return $this->agility;
+    }
+
+    public function setAgility(int $agility): static
+    {
+        $this->agility = $agility;
+
+        return $this;
+    }
+
+    public function getIntelligence(): ?int
+    {
+        return $this->intelligence;
+    }
+
+    public function setIntelligence(int $intelligence): static
+    {
+        $this->intelligence = $intelligence;
+
+        return $this;
+    }
+
+    public function getGender(): ?string
+    {
+        return $this->gender;
+    }
+
+    public function setGender(string $gender): static
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    public function getExperience(): ?string
+    {
+        return $this->experience;
+    }
+
+    public function setExperience(int $experience): static
+    {
+        $this->experience = $experience;
+
+        return $this;
+    }
+
+}
